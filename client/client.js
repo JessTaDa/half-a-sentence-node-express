32b93fa61d82f787f1235081136d8a5885e4d0f6
JESSTADA <jesstada@gmail.com>
2019-02-16 17:54:03 +1100

    display and reset form after submit

--- client/client.js
+++ client/client.js
@@ -1,18 +1,35 @@
 console.log('hello world');
 
-const form = document.querySelector('form'); //can also use getElementById
-const loadingElement = document.querySelector('.loading');
+const form = document.getElementById('form');
+const loadingElement = document.getElementById('loading');
+const API_URL = 'http://localhost:5000/sentences';
 
 loadingElement.style.display = 'none';
-
+console.log(form);
 form.addEventListener('submit', (event) => {
+  console.log('hello');
   event.preventDefault();
   const formData = new FormData(form);
   const sentenceTail = formData.get('sentenceTail');
-  console.log('frm submitted');
+
+  const sentence = {
+    sentenceTail
+  };
 
   form.style.display = 'none';
   loadingElement.style.display = '';
 
-
-})
+  fetch(API_URL, {
+    method: 'POST',
+    body: JSON.stringify(sentence),
+    headers: {
+      'content-type': 'application/json'
+    }
+  }).then(response => response.json())
+    .then(createdSentence => {
+      console.log(createdSentence);
+      form.reset();
+      form.style.display = '';
+      loadingElement.style.display = 'none';
+    });
+});
