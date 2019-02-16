2f74fa2b9a07672bd0d64842475f31a9493583fe
JESSTADA <jesstada@gmail.com>
2019-02-16 17:18:26 +1100

    sentenceTail sent to backend server successfully

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors()); //installs cors as middleware
+app.use(express.json()); //body parsor for anything from client of content-type: application/json
 
 app.get('/', (req, res) => {
   res.json({
@@ -8,6 +12,9 @@ app.get('/', (req, res) => {
   });
 });
 
+app.post('/sentence', (req, res) => {
+  console.log(req.body);
+});
 
 app.listen(5000, () => {
   console.log('listening on localhost:5000')
